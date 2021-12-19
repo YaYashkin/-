ada6f103bbe0aadd7f105fbaa96be8a05a64dc07
Миша Богатырёв <misha_bog100@inbox.ru>
2021-12-19 17:09:11 +0800

    Добавьте файлы проекта.

---
+++ Курсач/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Курсач
+{
+    public partial class Form1 : Form
+    {
+        List<Particle> particles = new List<Particle>();//Список частиц
+        public Form1()
+        {
+            InitializeComponent();
+
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);//Привязка изображения
+
+            for (var i = 0; i < 500; ++i) //Генерация 500 частиц
+            {
+                var particle = new Particle();
+                particle.X = picDisplay.Image.Width / 2; //Перенос частиц 
+                particle.Y = picDisplay.Image.Height / 2;              // в центр изображения
+
+                particles.Add(particle); //Добавление списка
+            }
+        }
+
+        public void UpdateState() //Обновление логики
+        {
+            foreach(var particle in particles) //пересчитывание положения частиц в соответствии с их направлением движения и скоростью.
+            {
+                particle.Life -= 1;//Уменьшение здоровья
+                if (particle.Life < 0)
+                {
+                    particle.Life = 20 + Particle.rand.Next(100);//Восстановление здоровья
+                    particle.X = picDisplay.Image.Width / 2; //Перенос частиц 
+                    particle.Y = picDisplay.Image.Height / 2;              // в центр изображения
+
+                    particle.Direction = Particle.rand.Next(360);//рандомное направление движения
+                    particle.Speed = 1 + Particle.rand.Next(10);//рандомная скорость
+                    particle.Radius = 2 + Particle.rand.Next(10);//рандомный радиус
+
+                }
+                else
+                {
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+        }
+
+        public void Render(Graphics g) //Отрисовка
+        {
+            foreach (var particle in particles) //отрисовка частиц
+            {
+                particle.Draw(g);
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateState();//Каждый тик обновляет систему
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White);//Очистка экрана, заливка формы в белый цвет
+                Render(g); //Рендеринг системы
+            }
+            picDisplay.Invalidate();//обновление формы
+        }
+    }
+}
---
+++ Курсач/Particle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing; //Библиотека для использованися Graphics
+
+namespace Курсач
+{
+    public class Particle //Класс частиц
+    {
+        public int Radius; //Радиус частицы
+        public float X; //X координата положения частицы в пространстве
+        public float Y; //Y координата положения частицы в пространстве
+        public float Direction; //Направление движения
+        public float Speed; //Скорость перемещения
+        public float Life; // здоровье частицы
+        public static Random rand = new Random();//Генератор случайных чисел
+
+        public Particle() //Констуруктор, который создаёт кастомную частицу
+        {
+            Direction = rand.Next(360);//
+            Speed = 1 + rand.Next(10);// Скорость от 1 до 11
+            Radius = 2 + rand.Next(10);// Радиус от 2 до 12
+            Life = 20 + rand.Next(100);// Запас здоровья от 20 до 120
+        }
+
+        public void Draw(Graphics g)
+        {
+            float k = Math.Min(1f, Life / 100);//коэф прозрачности от 0 до 1.0
+            int alpha = (int)(k * 255);//значение альфа от 0 до 255
+                                       //задание прозрачности
+            var color = Color.FromArgb(alpha, Color.Black);//привязываем к увету прозрачность
+
+            var b = new SolidBrush(color); //Кисть для рисования
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);//Создание залитого круга с радиусом и центром X, Y
+            b.Dispose();//Сборщик мусора, удаление кисти из памяти
+        }
+    }
+}
